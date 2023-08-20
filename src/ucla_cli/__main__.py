50ea52f78889250f911fea1fe69b7b9189008446
David Davini <dndavini3.14@gmail.com>
2023-08-20 12:40:20 -0700

    hacker time mode, location codes

--- src/ucla_cli/__main__.py
+++ src/ucla_cli/__main__.py
@@ -10,6 +10,11 @@ from ucla_cli.course_titles_view import course_titles_view
 from ucla_cli.get_course_summary import get_course_summary
 from ucla_cli.results import results
 
+def extract_location(soup):
+    p = soup.find_all(class_="locationColumn")[1].find("p")
+    if p.button:
+        return p.button.text.strip()
+    return p.text.strip()
 
 def extract_course_summary(soup):
     status_data = soup.find_all(class_="statusColumn")[1].find("p")
@@ -17,11 +22,38 @@ def extract_course_summary(soup):
         "status": [x for x in status_data.contents if isinstance(x, NavigableString)],
         "waitlist": soup.find_all(class_="waitlistColumn")[1].find("p").contents[0],
         "day": soup.find_all(class_="dayColumn")[1].find("p").text,
-        "time": soup.find_all(class_="timeColumn")[1].find_all("p")[1].text,
-        "location": soup.find_all(class_="locationColumn")[1].find("p").contents[0].strip(),
+        "time": [x for x in soup.find_all(class_="timeColumn")[1].find_all("p")[1].contents if isinstance(x, NavigableString)],
+        "location": extract_location(soup),
         "units": soup.find_all(class_="unitsColumn")[1].find("p").contents[0],
         "instructor": soup.find_all(class_="instructorColumn")[1].find("p").contents[0],
     }
+
+def clean_time(time):
+    if time == []:
+        return ""
+    if time == ['To be arranged']:
+        return "?"
+    if time == ['-','-','-']:
+        return "?"
+    if len(time) % 2 != 0:
+        raise ValueError
+    intervals = zip(time[::2],time[1::2])
+    times = []
+    def parse_time(t):
+        m = re.match("(\d+):?(\d*)([pa])m", t)
+        if not m:
+            raise ValueError
+        h = int(m.group(1))
+        mins = m.group(2)
+        mins = int(mins if mins else 0)
+        p = 12 if (m.group(3)=='p' and h != 12) else 0
+        return h+p+mins/60
+    for start, end in intervals:
+        s = parse_time(start)
+        e = parse_time(end[1:])
+        s,e = [round(x) for x in [s,e]]
+        times.extend(list(range(s,e)))
+    return "".join(c if i in times else '.' for i, c in enumerate('89ABC1234567', 8))
 
 def clean_status_code(status_code):
     return {
@@ -74,6 +106,8 @@ def clean_waitlist(waitlist):
 def clean_day(day):
     if day == "Not scheduled":
         return ""
+    if day == "Varies":
+        return day
     if day.upper() != day or " " in day:
         raise ValueError
     return "".join([c if c in day else "." for c in "UMTWRFS"])
@@ -83,12 +117,18 @@ def clean_instructor(instructor):
     #    return ''
     return instructor
 
-def clean_location(location):
+def clean_location(location, locations):
     #if location == "No Location":
     #    return ''
-    return location
+    ms = {l: re.match(l, location) for l in locations}
+    matches = {l: m for l,m in ms.items() if m}
+    if len(matches) != 1:
+        raise ValueError
+    l, m = matches.popitem()
+    building, room = location[:m.end()], location[m.end():]
+    return locations[l] + room
 
-def clean_course_summary(data):
+def clean_course_summary(data, filters):
     num_enrolled, total_spots = clean_status(data["status"])
     num_waitlisted, waitlist_capacity = clean_waitlist(data["waitlist"])
     data.update(
@@ -99,8 +139,8 @@ def clean_course_summary(data):
             "num_waitlisted": num_waitlisted,
             "waitlist_capacity": waitlist_capacity,
             "day": clean_day(data["day"]),
-            "time": data["time"],
-            "location": clean_location(data["location"]),
+            "time": clean_time(data["time"]),
+            "location": clean_location(data["location"], filters['location']),
             "instructor": clean_instructor(data["instructor"]),
             "units": data["units"],
         }
@@ -144,6 +184,16 @@ def soc(args):
     subject_code_table = {reduce_subject(x["value"]): x["value"] for x in subject_table}
     subject_name = subject_name_table[reduce_subject(args.subject)]
     subject = subject_code_table[reduce_subject(args.subject)]
+    # we call results() again with our "main search field"
+    # this is just to get the filter options, not the course list
+    # but we call course_titles_view() for purely unfiltered course list
+    text = results(args.term, subject)
+    soup = BeautifulSoup(text, 'html.parser')
+    locations = soup.select("#Location_options option")
+    locations = {l.contents[0]: l['value'] for l in locations}
+    filters = {
+        'location': locations
+    }
     columns = [
         Column("subject", "{:<7}"),
         Column("numb", "{:<5}"),
@@ -157,8 +207,8 @@ def soc(args):
                 Column("wai", "{:>3}"),
                 Column("wcp", "{:>3}"),
                 Column("days", "{:<7}"),
-                Column("times", "{:<22}"),
-                Column("location", "{:<31}"),
+                Column("times", "{:<13}"),
+                Column("location", "{:<14}"),
                 Column("uni", "{:>3}"),
                 Column("instructor", "{:<20}"),
             ]
@@ -184,7 +234,7 @@ def soc(args):
                 sum_soup = get_course_summary(model)
                 data = extract_course_summary(sum_soup)
                 orig_data = data.copy()
-                data = clean_course_summary(data)
+                data = clean_course_summary(data, filters)
             row = [subject, number]
             if args.course_details:
                 row.extend(
