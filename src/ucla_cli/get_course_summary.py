122f32e669b3fbb2d4cda68dca0a560e84b87d3f
David Davini <dndavini3.14@gmail.com>
2023-08-19 19:56:53 -0700

    fix impacted class error, check for another type of error

--- src/ucla_cli/get_course_summary.py
+++ src/ucla_cli/get_course_summary.py
@@ -1,13 +1,22 @@
 import json
+from urllib.parse import parse_qs
 
 import requests
 from bs4 import BeautifulSoup
 
+def _decode_url(url):
+    url, params = url.split("?")
+    params = parse_qs(params)
+    params = {key: [json.loads(v) for v in value] for key, value in params.items()}
+    return params
+
+def prep_params(params):
+    return {key: [json.dumps(v) for v in value] for key, value in params.items()}
+
 def get_course_summary(model): 
-    params = {
-        "model": [json.dumps(model)],
+    raw_params = {
+        "model": [model],
         "FilterFlags": [
-            json.dumps(
                 {
                     "enrollment_status": "O,W,C,X,T,S",
                     "advanced": "y",
@@ -18,24 +27,27 @@ def get_course_summary(model):
                     "meet_units": None,
                     "instructor": None,
                     "class_career": None,
-                    "impacted": "N",
+                    "impacted": None,
                     "enrollment_restrictions": None,
                     "enforced_requisites": None,
                     "individual_studies": "n",
                     "summer_session": None,
                 }
-            )
         ],
-        "_": ["1692258949331"],
+        "_": [1692258949331],
     }
+    params = prep_params(raw_params)
     url = "https://sa.ucla.edu/ro/public/soc/Results/GetCourseSummary"
     resp = requests.get(url, params)
     soup = BeautifulSoup(resp.text, "html.parser")
-    error = soup.find(class_='expanded-error-message')
-    if error:
-        raise Exception(error.text)
     if not soup.text:
         raise Exception
+    error = soup.find(class_='expanded-error-message')
+    if error:
+        raise Exception(error.text.strip())
+    error = soup.find(class_='error_section')
+    if error:
+        raise Exception(error.text.strip())
     return soup
 
 
