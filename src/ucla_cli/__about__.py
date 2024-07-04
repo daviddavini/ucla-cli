38e0a92f401843fb0d4bbba09ace44922295eb8c
David Davini <dndavini3.14@gmail.com>
2024-07-04 01:59:00 -0700

    switch to poetry

--- src/ucla_cli/__about__.py
+++
@@ -1,4 +0,0 @@
-# SPDX-FileCopyrightText: 2023-present David Davini <dndavini3.14@gmail.com>
-#
-# SPDX-License-Identifier: MIT
-__version__ = "0.0.8"
--- src/ucla_cli/__init__.py
+++ src/ucla_cli/__init__.py
@@ -1,5 +1,1 @@
-# SPDX-FileCopyrightText: 2023-present David Davini <dndavini3.14@gmail.com>
-#
-# SPDX-License-Identifier: MIT
-
 from ucla_cli.__main__ import ucla
